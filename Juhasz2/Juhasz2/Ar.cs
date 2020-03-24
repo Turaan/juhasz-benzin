d21ecb15723fb3e1b892f9745db9ed3b8b4445fc
Turaan <48063665+Turaan@users.noreply.github.com>
2020-03-24 12:40:26 +0100

    Add files via upload

---
+++ Juhasz2/Juhasz2/Ar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juhasz2
+{
+    class Ar
+    {
+       
+            public DateTime Valtozas { get; set; }
+            public int Benzin { get; set; }
+            public int Gazolaj { get; set; }
+
+        public int Diferencial => Math.Abs(Benzin - Gazolaj);
+
+        public bool Szokonap => DateTime.IsLeapYear(Valtozas.Year)
+            && Valtozas.Month == 2
+            && Valtozas.Day == 24;
+
+
+        public Ar(string s)
+        {
+            var t = s.Split(';');
+            Valtozas = DateTime.Parse(t[0]);
+            Benzin = int.Parse(t[1]);
+            Gazolaj = int.Parse(t[2]);
+        }
+
+    }
+}
---
+++ Juhasz2/Juhasz2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Juhasz2
+{
+    
+
+
+    class Program
+    {
+        static List<Ar> arak;
+        static int minv;
+        const float euro = 350.23F;
+        static int ev;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F03();
+            F04();
+            F05();
+            F06();
+            F07();
+            F08();
+            Console.ReadKey();
+        }
+        private static void F03()
+        {
+            Console.WriteLine($"f3:{arak.Count}");
+        }
+        private static void F04()
+        {
+            int minv = arak.Min(a => Math.Abs(a.Benzin - a.Gazolaj));
+
+            minv = arak.Min(a => a.Diferencial);
+            Console.WriteLine("legkisebb kül.:" + minv);
+        }
+
+
+        private static void F05()
+        {
+            int c = arak.Count(a => Math.Abs(a.Benzin - a.Gazolaj) == arak.Min(b => Math.Abs(a.Benzin - a.Gazolaj)));
+            c = arak.Count(a => a.Diferencial == minv);
+            Console.WriteLine($"ennyiszer volt:{c}");
+        }
+
+        private static void F06()
+        {
+            bool szkn = arak.Any(a => a.Valtozas.Year % 4 == 0
+            && a.Valtozas.Month == 2
+            && a.Valtozas.Day == 24);
+
+            szkn = arak.Any(a => a.Szokonap);
+
+            Console.WriteLine((szkn?"van":"nincs")+"szökőnapon változás");
+        }
+        private static void F07()
+        {
+            var sw = new StreamWriter(@"");
+        }
+        private static void F08()
+        {
+            do
+            {
+                Console.Write("ev:");
+                ev = int.Parse(Console.ReadLine());
+            } while (ev < 2011 || ev > 2016);
+
+        }
+        private static void Beolvas()
+        {
+            arak = new List<Ar>();
+            var sr = new StreamReader(@"C:\Users\Turan\OneDrive\Asztali gép\Juhasz2\Juhasz2\bin\Debug\uzemanyagg.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                arak.Add(new Ar(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+    }
+}
